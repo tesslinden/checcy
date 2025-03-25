e41460732a4f94628fd2940388c5f6949be2147a
Tess Linden <tlinden@bighatbio.com>
2025-03-24 21:21:48 -0700

    actually switch between local and prod reccy

--- background.js
+++ background.js
@@ -1,10 +1,13 @@
 chrome.action.onClicked.addListener((tab) => {
-  // 1. Extract the current URL
   const currentUrl = tab.url;
-
-  // 2. Replace "foo" with "bar"
-  const modifiedUrl = currentUrl.replace(/reccy/g, "test");
-
-  // 3. Navigate the current tab to the new URL
+  const isLocalReccy = currentUrl.includes("local.bighatbio.me:3000");
+  const isProdReccy = currentUrl.includes("reccy.bighatbio.com");
+  if (!isLocalReccy && !isProdReccy) {
+    return;
+  }
+  const modifiedUrl = isLocalReccy
+      ? currentUrl.replace("local.bighatbio.me:3000", "reccy.bighatbio.com")
+      : currentUrl.replace("reccy.bighatbio.com", "local.bighatbio.me:3000");
+  // Navigate the current tab to the new URL
   chrome.tabs.update(tab.id, { url: modifiedUrl });
 });
