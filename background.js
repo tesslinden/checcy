e332c982aa1e60ac2f2633942f05f21404edd53b
Tess Linden <tlinden@bighatbio.com>
2025-03-24 21:36:38 -0700

    open new tab instead of navigate within current tab

--- background.js
+++ background.js
@@ -1,13 +1,19 @@
 chrome.action.onClicked.addListener((tab) => {
-  const currentUrl = tab.url;
-  const isLocalReccy = currentUrl.includes("local.bighatbio.me:3000");
-  const isProdReccy = currentUrl.includes("reccy.bighatbio.com");
-  if (!isLocalReccy && !isProdReccy) {
-    return;
-  }
-  const modifiedUrl = isLocalReccy
-      ? currentUrl.replace("local.bighatbio.me:3000", "reccy.bighatbio.com")
-      : currentUrl.replace("reccy.bighatbio.com", "local.bighatbio.me:3000");
-  // Navigate the current tab to the new URL
-  chrome.tabs.update(tab.id, { url: modifiedUrl });
+   const localReccyURL = "http://local.bighatbio.me:3000";
+   const prodReccyURL = "https://reccy.bighatbio.com";
+   const currentUrl = tab.url;
+   const isLocalReccy = currentUrl.includes(localReccyURL);
+   const isProdReccy = currentUrl.includes(prodReccyURL);
+   if (!isLocalReccy && !isProdReccy) {
+     return;
+   }
+   const modifiedUrl = isLocalReccy
+       ? currentUrl.replace(localReccyURL, prodReccyURL)
+       : currentUrl.replace(prodReccyURL, localReccyURL);
+   // Navigate the current tab to the new URL
+   // chrome.tabs.update(tab.id, { url: modifiedUrl });
+
+   // Open modified URL in a new tab
+   chrome.tabs.create({ url: modifiedUrl });
+
 });
